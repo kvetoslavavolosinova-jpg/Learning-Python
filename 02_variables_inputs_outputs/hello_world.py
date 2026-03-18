ef0266d51f2767101eb6f84144526b5b69e89f7c
Kvetka <Kvetka@device-95.home>
2026-03-18 16:32:08 +0100

    removed 01_intro folder

---
+++ 02_variables_inputs_outputs/hello_world.py
@@ -0,0 +1,10 @@
+# Získame meno a vek od používateľa
+meno = input("Zadaj svoje meno: ")
+vek = int(input("Zadaj svoj vek: "))
+
+# Výpis pozdravu a informácie o veku
+print("Ahoj", meno, "!")
+print("Budúci rok budeš mať", vek + 1, "rokov")
+
+meno=input("Ako sa volas?")
+print("Ahoj", meno)
